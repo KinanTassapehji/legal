274d6bed519102835bc277d7cf0dfbaf85b265c6
nibin <nibin@mobilityeye.com>
2024-05-01 16:23:15 +0530

    feat(LIC-175): As a Designer, I want to design create License popup

---
+++ MBE.LegalPortal.UI/src/app/components/license/create-license/create-license.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-license',
+  templateUrl: './create-license.component.html',
+  styleUrl: './create-license.component.scss'
+})
+export class CreateLicenseComponent {
+
+}
