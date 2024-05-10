81f73b48746548845fcfe172380ad74a2d6b5ecb
suleman waseem <suleman@mobilityeye.com>
2024-05-10 09:41:14 +0400

    feat(LIC-194): create a list view for license with searching, sorting, and pagination.

--- MBE.LegalPortal.UI/src/app/interfaces/license.ts
+++ MBE.LegalPortal.UI/src/app/interfaces/license.ts
@@ -1,9 +1,15 @@
+import { IAccount } from "./account";
+import { IApplication } from "./application";
 import { ISubscriptionPlan } from "./subscription-plan";
+import { ITenant } from "./tenant";
 
 export interface ILicense {
   id: number,
   environment: string,
   expiryDate: Date,
   expiryAction: string,
-  subscriptionPlan: ISubscriptionPlan
+  subscriptionPlan: ISubscriptionPlan,
+  tenant: ITenant,
+  account: IAccount,
+  application: IApplication
 }
