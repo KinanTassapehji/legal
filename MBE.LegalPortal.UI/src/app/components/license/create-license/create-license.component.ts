89d78668e611d33352b95759c840b3a30c2299ad
suleman waseem <suleman@mobilityeye.com>
2024-05-15 15:55:30 +0400

    feat(LIC-191): add new feature to create new license in legal portal

--- MBE.LegalPortal.UI/src/app/components/license/create-license/create-license.component.ts
+++ MBE.LegalPortal.UI/src/app/components/license/create-license/create-license.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { LicenseService } from '../../../services/license.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
+import { IApplication } from '../../../interfaces/application';
+import { IAccount } from '../../../interfaces/account';
+import { IApplicationInstance } from '../../../interfaces/application-instance';
+import { ITenant } from '../../../interfaces/tenant';
+import { ISubscriptionPlan } from '../../../interfaces/subscription-plan';
 
 @Component({
   selector: 'app-create-license',
@@ -6,5 +14,115 @@ import { Component } from '@angular/core';
   styleUrl: './create-license.component.scss'
 })
 export class CreateLicenseComponent {
+  sub!: Subscription
+  @Output() licenseAdded: EventEmitter<void> = new EventEmitter<void>();
+  applications: IApplication[] = [];
+  accounts: IAccount[] = [];
+  applicationInstance: IApplicationInstance[] = [];
+  tenants: ITenant[] = [];
+  subscriptionPlans: ISubscriptionPlan[] = [];
+  applicationId: number = 0;
+  accountId: number = 0;
+  applicationInstanceId: number = 0;
+  tenantId: number = 0;
+  subscriptionPlanId: number = 0;
+  environment: string = '';
+  expiryDate: string = '';
+  expiryAction: string = '';
+  applicationConstraints: any[] = [];
+  constructor(private licenseService: LicenseService, private dialogRef: MatDialogRef<CreateLicenseComponent>) { }
 
+  ngOnInit(): void {
+    this.getApplications();
+  }
+
+  addLicense() {
+    let requestBody = {
+      "expiryDate": this.expiryDate,
+      "expiryAction": this.expiryAction,
+      "environment": this.environment,
+      "tenantId": this.tenantId,
+      "subscriptionPlanId": this.subscriptionPlanId,
+      "Constraints": this.applicationConstraints
+    };
+    this.sub = this.licenseService.createLicense(requestBody).subscribe({
+      next: (response) => {
+        // Emit event to notify parent component
+        this.licenseAdded.emit(response.data);
+        // Close the dialog
+        this.dialogRef.close();
+      },
+      error: (err) => {
+        console.log('err', err);
+        // Handle error response, maybe show an error message
+        console.error('Error creating application instance', err);
+      }
+    });
+  }
+
+  OnDateChange(value: any) {
+    this.expiryDate = value;
+  }
+  getApplications() {
+    this.sub = this.licenseService.getApplications().subscribe({
+      next: response => {
+        this.applications = response.data;
+        this.getAccounts();
+        this.getSubscriptionPlans();
+      }
+    });
+  }
+
+  getAccounts() {
+    this.sub = this.licenseService.getAccounts().subscribe({
+      next: response => {
+        this.accounts = response.data;
+      }
+    });
+  }
+
+  getSubscriptionPlans() {
+    this.sub = this.licenseService.getSubscriptionPlans().subscribe({
+      next: response => {
+        this.subscriptionPlans = response.data;
+      }
+    });
+  }
+
+  getApplicationInstance() {
+    this.applicationInstance = [];
+    this.tenants = [];
+    if (this.accountId > 0 && this.applicationId > 0) {
+        this.sub = this.licenseService.getApplicationInstance(this.accountId, this.applicationId).subscribe({
+        next: response => {
+          if (response.data.length > 0) {
+            this.applicationInstance = [response.data[0].application];
+            this.tenants = response.data[0].tenants;
+          }
+        }
+      });
+    }
+  }
+
+  getApplicationConstraints() {
+    this.applicationConstraints = [];
+    this.sub = this.licenseService.getApplicationConstraints(this.subscriptionPlanId).subscribe({
+      next: response => {
+        let applicationConstaintsList = response.data.applicationConstraints;
+        if (applicationConstaintsList.length > 0) {
+          for (var i = 0; i < applicationConstaintsList.length; i++) {
+            let action = applicationConstaintsList[i].defaultAction;
+            let applicationConstraintId = applicationConstaintsList[i].id;
+            let value = applicationConstaintsList[i].defaultValue;
+            let Constraints = {
+              "applicationConstraintId": applicationConstraintId,
+              "value": value,
+              "action": action
+            };
+            this.applicationConstraints.push(Constraints);
+          }
+        }
+      }
+    });
+  }
 }
--- MBE.LegalPortal.UI/src/app/services/license.service.ts
+++ MBE.LegalPortal.UI/src/app/services/license.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, map } from 'rxjs';
-import { License_Url } from '../constants/apis-constants';
+import {
+  License_Url,
+  Applications_Url,
+  Accounts_Url,
+  Application_Instances_Url,
+  SubscriptionPlans_Url,
+} from '../constants/apis-constants';
 import { ErrorHandlingService } from './error-handling-service';
 import { ILicense } from '../interfaces/license';
 
@@ -19,5 +25,49 @@ export class LicenseService {
         map(response => response.data),
         catchError(error => this.errorHandlingService.handleError(error))
       );
+  }
+  createLicense(data: any): Observable<any> {
+    return this.http.post(License_Url, data).pipe(catchError(error => this.errorHandlingService.handleError(error)));
+  }
+  getLicense(page: number, pageSize: number, sort?: Sort, keyword?: string): Observable<any> {
+    // Constructing query parameters
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('pageSize', pageSize.toString());
+
+    if (sort?.direction !== undefined && sort?.direction !== '') {
+      const direction = sort.direction === 'asc' ? 'Asc' : 'Desc';
+      params = params.set('sortDirection', direction);
+    }
+
+    if (sort?.active !== undefined) {
+      params = params.set('orderBy', sort.active);
+    }
+
+    if (keyword !== undefined && keyword.trim() !== '') {
+      params = params.set('keyword', keyword.trim());
+    }
+    return this.http.get<any>(`${License_Url}`, { params })
+      .pipe(catchError(error => this.errorHandlingService.handleError(error)));
+  }
+  getApplications(): Observable<any> {
+    return this.http.get<any>(`${Applications_Url}`)
+      .pipe(catchError(error => this.errorHandlingService.handleError(error)));
+  }
+  getAccounts(): Observable<any> {
+    return this.http.get<any>(`${Accounts_Url}`)
+      .pipe(catchError(error => this.errorHandlingService.handleError(error)));
+  }
+  getSubscriptionPlans(): Observable<any> {
+    return this.http.get<any>(`${SubscriptionPlans_Url}`)
+      .pipe(catchError(error => this.errorHandlingService.handleError(error)));
+  }
+  getApplicationInstance(accountId: number, applicationId: number): Observable<any> {
+    return this.http.get<any>(`${Application_Instances_Url}?ApplicationId=${applicationId}&AccountId=${accountId}`)
+      .pipe(catchError(error => this.errorHandlingService.handleError(error)));
+  }
+  getApplicationConstraints(SubscriptionPlanId: number): Observable<any> {
+    return this.http.get<any>(`${SubscriptionPlans_Url}/${SubscriptionPlanId}`)
+      .pipe(catchError(error => this.errorHandlingService.handleError(error)));
   }
 }
