0d5b906c486d907fee0ad4ef79efb216e6169c1b
Kinan Tassapehji <kinan@mobilityeye.com>
2025-10-08 09:53:26 +0000

    Merged in Kinan-Tassapehji/apisconstantsts-edited-online-with-bitbu-1759917191707 (pull request #132)
    apis-constants.ts edited online with Bitbucket

--- MBE.LegalPortal.UI/src/app/constants/apis-constants.ts
+++ MBE.LegalPortal.UI/src/app/constants/apis-constants.ts
@@ -1,5 +1,5 @@
-// export const BaseUrl: string = 'https://legal-dev.mobilityeye.com/api/api/';
-export const BaseUrl: string = 'https://localhost:7147/api/';
+//export const BaseUrl: string = 'https://localhost:7147/api/';
+export const BaseUrl: string = 'https://legal-dev.mobilityeye.com/api/api/';
 export const SubscriptionPlans_Url: string = BaseUrl + 'SubscriptionPlan';
 export const Applications_Url: string = BaseUrl + 'Application';
 export const Application_Instances_Url: string = BaseUrl + 'ApplicationInstance';
